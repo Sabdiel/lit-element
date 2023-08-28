ccb3879b24c5ea44da295fe7eabf33528b88c92a
Sabdiel GM <sabdielgutierrez@gmail.com>
2023-08-28 09:59:42 -0600

    actualización de funciones

--- src/simple-component.js
+++ src/simple-component.js
@@ -1,31 +1,61 @@
 import { LitElement,css, html } from 'lit';
 import {map} from 'lit/directives/map.js';
 export class SimpleComponent extends LitElement {
-  static properties = {
-    things: {state: true},
-  };
-_deleteThing() {
-    this.things = /*this.things.pop((_, i) => i !== index)*/ console.log('enter');
-  }
-  constructor() {
-    super();
-    this.lista = ['Lista', ];
-  }
-  render() {
-    return html`
-      <input @keyup="${this.agregarElemento}">
-      <ul>
-        ${this.lista.map(item => html`<li>${item}</li>`)}
-      </ul>
-    `;
+  static get properties() {
+    return {
+      tasks: { type: Array },
+      inputText: { type: String },
+    };
   }
 
-  agregarElemento(e) {
-    if (e.key === 'Enter') {
-      this.lista = [...this.lista, e.target.value];
-      e.target.value = '';
+  constructor() {
+    super();
+    this.tasks = [];
+    this.inputText = '';
+  }
+
+  nuevaTarea() {
+    if (this.inputText) {
+      this.tasks = [...this.tasks, { text: this.inputText, done: false,timeAdded:Date.now()}];
+      this.inputText = '';
     }
   }
 
+  marcarTarea(index) {
+    this.tasks = this.tasks.map((task, i) => {
+      if (i === index) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    });
+  }
+
+  render() {
+    return html`
+      <input
+        type="text"
+        .value=${this.inputText}
+        @keyup=${(e) => e.key === 'Enter' && this.nuevaTarea()}
+        @input=${(e) => (this.inputText = e.target.value)}
+      />
+      <ul>
+        ${this.tasks.map(
+          (task, i) =>
+            html`
+              <li style="text-decoration: ${task.done ? 'line-through' : 'none'};
+               color: ${task.done ? 'gray' : 'black'}; 
+               background-color: ${Date.now() - task.timeAdded > 5000 ? 'red' : 'green'};">
+                <input
+                  type="checkbox"
+                  .checked=${task.done}
+                  @change=${() => this.marcarTarea(i)}
+                />
+                ${task.text}
+              </li>
+            `
+        )}
+      </ul>
+    `;
+  }
 }
 customElements.define('simple-component', SimpleComponent);
